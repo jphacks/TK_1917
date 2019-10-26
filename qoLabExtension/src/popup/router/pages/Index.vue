214d5ea2438abedc427bdeb948f909002c1b3757
wally <u.str.gm@gmail.com>
2019-10-26 11:11:48 +0900

    feat: ログインボタン押したらHomeに行けるようにルーター仕込んだ

--- qoLabExtension/src/popup/router/pages/Index.vue
+++ qoLabExtension/src/popup/router/pages/Index.vue
@@ -1,17 +1,28 @@
 <template>
-  <p>Hello world!</p>
+  <div>
+    <h2>ログイン</h2>
+    <form>
+      <label for="email">Email</label>
+      <input type="email" name="email" />
+      <label for="password">Password</label>
+      <input type="password" name="password" />
+      <input type="submit" v-on:click="login" />
+    </form>
+  </div>
 </template>
 
 <script>
+import router from 'vue-router';
 export default {
   data() {
     return {};
   },
+  methods: {
+    login: function() {
+      this.$router.push('/home');
+    },
+  },
 };
 </script>
 
-<style lang="scss" scoped>
-p {
-  font-size: 20px;
-}
-</style>
+<style lang="scss" scoped></style>
--- qoLabExtension/src/popup/router/routes.js
+++ qoLabExtension/src/popup/router/routes.js
@@ -1,8 +1,13 @@
 import PageIndex from './pages/Index';
+import Home from './pages/Home';
 
 export default [
   {
     path: '/',
     component: PageIndex,
+  },
+  {
+    path: '/home',
+    component: Home,
   },
 ];
