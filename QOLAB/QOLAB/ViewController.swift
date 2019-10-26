43abf9bb5c56386ab453bc80197f5702c2e91511
mr04vv <mr04vv@gmail.com>
2019-10-26 10:37:26 +0900

    add: xcode project

---
+++ QOLAB/QOLAB/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  QOLAB
+//
+//  Created by Takuto Mori on 2019/10/26.
+//  Copyright © 2019 Takuto Mori. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
