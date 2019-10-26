59b1c1915f2ae67116729902a492af6181889f7f
mr04vv <mr04vv@gmail.com>
2019-10-26 16:01:27 +0900

    add: allow http setting

---
+++ QOLAB/QOLAB/Data/Api/apiClient.swift
@@ -0,0 +1,9 @@
+//
+//  apiClient.swift
+//  QOLAB
+//
+//  Created by Takuto Mori on 2019/10/26.
+//  Copyright © 2019 Takuto Mori. All rights reserved.
+//
+
+import Foundation
---
+++ QOLAB/QOLAB/Data/Entity/ReponseDto/playgroundResponse.swift
@@ -0,0 +1,9 @@
+//
+//  playgroundResponse.swift
+//  QOLAB
+//
+//  Created by Takuto Mori on 2019/10/26.
+//  Copyright © 2019 Takuto Mori. All rights reserved.
+//
+
+import Foundation
