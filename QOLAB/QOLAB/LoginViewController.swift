a16b9e56d4f03ccaf36e408c4799e621f97ad031
mr04vv <mr04vv@gmail.com>
2019-10-26 11:50:51 +0900

    add: login form

--- QOLAB/QOLAB/LoginViewController.swift
+++ QOLAB/QOLAB/LoginViewController.swift
@@ -7,3 +7,21 @@
 //
 
 import Foundation
+import Cocoa
+
+class LoginViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
