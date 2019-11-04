e75ea05e3176e0e097eb481c3d6d7a03b721dccb
TakutoMori <mr04vv@gmail.com>
2019-11-05 04:23:57 +0900

    feat: member observer monitor

--- QOLAB/QOLAB/Monitor/MemberObserver.swift
+++ QOLAB/QOLAB/Monitor/MemberObserver.swift
@@ -6,4 +6,50 @@
 //  Copyright © 2019 Takuto Mori. All rights reserved.
 //
 
+import Cocoa
 import Foundation
+
+class MemberObserver: NSObject, NSApplicationDelegate, NSUserNotificationCenterDelegate {
+    let TIMER_NORMAL_SEC = 5.0
+    
+    var count = 1
+    
+    var timerNormal = Timer()
+    
+    var appDelegate: AppDelegate?
+    
+    override init() {}
+    
+    func start() {
+        /* タイマー実行 */
+        timerNormal = Timer.scheduledTimer(
+            timeInterval: TIMER_NORMAL_SEC, // 実行する時間
+            target: self,
+            selector: #selector(CountDown), // 実行関数
+            userInfo: nil,
+            repeats: true
+        )
+    }
+    
+    /* タイマー関数 */
+    @objc func CountDown() {
+        fetchMembverActivity()
+    }
+    
+    @objc func fetchMembverActivity() {
+        APIClient.fetchMemberActivities { result in
+            DispatchQueue.main.sync {
+                self.appDelegate = NSApplication.shared.delegate as? AppDelegate
+                self.appDelegate?.initStatusBar()
+                self.appDelegate?.statusBarMenu.addItem(NSMenuItem.separator())
+                result?.members.forEach { member in
+                    self.appDelegate?.addMemberActivity(name: member.name, activity: member.activity)
+                }
+            }
+        }
+    }
+    
+    func quit() {
+        timerNormal.invalidate()
+    }
+}
