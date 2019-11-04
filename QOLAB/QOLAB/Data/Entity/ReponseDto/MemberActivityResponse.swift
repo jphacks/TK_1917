9eb1ff8127d9894782ea3ef1d7f16aa8379ddaef
TakutoMori <mr04vv@gmail.com>
2019-11-05 04:23:06 +0900

    add: some config files

---
+++ QOLAB/QOLAB/Data/Entity/ReponseDto/MemberActivityResponse.swift
@@ -0,0 +1,9 @@
+//
+//  MemberActivityResponse.swift
+//  QOLAB
+//
+//  Created by Takuto Mori on 2019/11/05.
+//  Copyright © 2019 Takuto Mori. All rights reserved.
+//
+
+import Foundation
---
+++ QOLAB/QOLAB/Monitor/MemberObserver.swift
@@ -0,0 +1,9 @@
+//
+//  MemberObserver.swift
+//  QOLAB
+//
+//  Created by Takuto Mori on 2019/11/05.
+//  Copyright © 2019 Takuto Mori. All rights reserved.
+//
+
+import Foundation
