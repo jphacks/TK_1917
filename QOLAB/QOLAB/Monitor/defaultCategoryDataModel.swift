da27d76f0dc3b0ba0c2887a78f7d22e84edab9c5
Om0m <em49amihso@gmail.com>
2019-11-07 06:38:35 +0900

    add defaultcategory.json

---
+++ QOLAB/QOLAB/Monitor/defaultCategoryDataModel.swift
@@ -0,0 +1,9 @@
+//
+//  defaultCategoryDataModel.swift
+//  QOLAB
+//
+//  Created by Oshima Masachika on 2019/11/07.
+//  Copyright © 2019 Takuto Mori. All rights reserved.
+//
+
+import Foundation
