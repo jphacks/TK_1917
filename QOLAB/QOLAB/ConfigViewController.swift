d9757b543b4000e6742c905cbb0d24999bde57cd
Om0m <em49amihso@gmail.com>
2019-11-08 13:24:50 +0900

    ログとコンフィグの画面を同じにした

--- QOLAB/QOLAB/ConfigViewController.swift
+++ QOLAB/QOLAB/ConfigViewController.swift
@@ -13,18 +13,10 @@ class ConfigViewController: NSViewController {
     let userDefaults = UserDefaults.standard
     var appDelegate: AppDelegate = NSApplication.shared.delegate as! AppDelegate
     
+    @IBOutlet weak var tableView: NSTableView!
     @IBOutlet weak var sittingTimer: NSTextField!
     @IBOutlet weak var normalTimer: NSTextField!
     @IBOutlet weak var sittingThreshold: NSTextField!
-    @IBAction func logButtonClicked(_ sender: Any) {
-        let storyboardName = NSStoryboard.Name(stringLiteral: "Config")
-        let storyboard = NSStoryboard(name: storyboardName, bundle: nil)
-        let storyboardID = NSStoryboard.SceneIdentifier(stringLiteral: "logStoryboardID")
-        
-        if let logWindowController = storyboard.instantiateController(withIdentifier: storyboardID) as? NSWindowController {
-            logWindowController.showWindow(nil)
-        }
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -47,12 +39,58 @@ class ConfigViewController: NSViewController {
         userDefaults.set(sittingTimer.stringValue, forKey: "sittingTimer")
         userDefaults.set(sittingThreshold.stringValue, forKey: "sittingThreshold")
         
-        appDelegate.closeConfigPopover(sender)
-        appDelegate.initStatusBar()
+        view.window?.close()
     }
     
     @IBAction func cancelButtonClicked(_ sender: NSButton) {
-        appDelegate.closeConfigPopover(sender)
-        appDelegate.initStatusBar()
+        view.window?.close()
+    }
+}
+
+extension ConfigViewController: NSTableViewDataSource {
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return Sensing.appLogList.count
+    }
+}
+
+extension ConfigViewController: NSTableViewDelegate {
+    fileprivate enum CellIdentifiers {
+        static let ApplicationNameCell = "ApplicationNameCellID"
+        static let DateCell = "DateCellID"
+        static let CategoryCell = "CategoryCellID"
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+//    var image: NSImage?
+        var text: String = ""
+        var cellIdentifier: String = ""
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = DateFormatter.dateFormat(fromTemplate: "ydMMMHms", options: 0, locale: Locale(identifier: "ja_JP"))
+        
+        // 1
+//    guard let item = directoryItems?[row] else {
+//      return nil
+//    }
+        
+        // 2
+        if tableColumn == tableView.tableColumns[0] {
+            text = dateFormatter.string(from: Sensing.dateList[row])
+            cellIdentifier = CellIdentifiers.DateCell
+        } else if tableColumn == tableView.tableColumns[1] {
+            text = Sensing.appLogList[row]
+            cellIdentifier = CellIdentifiers.ApplicationNameCell
+        } else if tableColumn == tableView.tableColumns[2] {
+            text = Sensing.categoryLogList[row]
+            cellIdentifier = CellIdentifiers.CategoryCell
+        }
+        
+        // 3
+        if let cell = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: cellIdentifier), owner: nil) as? NSTableCellView {
+            cell.textField?.stringValue = text
+//      cell.imageView?.image = image ?? nil
+            return cell
+        }
+        return nil
     }
 }
