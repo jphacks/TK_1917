5cb6ce7cefc25999ff4437528f98b397b735abb7
wally <u.str.gm@gmail.com>
2019-11-08 18:06:32 +0900

    feat: ユーザー名登録できるようにした

--- qoLabFrontEnd/pages/user_settings/index.vue
+++ qoLabFrontEnd/pages/user_settings/index.vue
@@ -1,7 +1,93 @@
 <template>
-  <div>user settings</div>
+  <v-layout column justify-center align-center>
+    <h2 class="text">ユーザー設定</h2>
+    <v-form
+      ref="form"
+      v-model="valid"
+      :lazy-validation="lazy"
+      class="form"
+      @submit.prevent="submit"
+    >
+      <v-text-field
+        v-model="name"
+        :rules="nameRules"
+        label="your name"
+        required
+      ></v-text-field>
+      <div class="submitButton">
+        <v-btn
+          v-if="!this.isLoading"
+          :disabled="!valid"
+          type="submit"
+          class="button"
+          color="primary"
+          >設定</v-btn
+        >
+        <v-btn
+          v-if="this.isLoading"
+          :disabled="!valid || this.isLoading"
+          type="submit"
+          class="button"
+          color="primary"
+        >
+          <v-progress-circular
+            indeterminate
+            color="primary"
+          ></v-progress-circular>
+        </v-btn>
+      </div>
+    </v-form>
+  </v-layout>
 </template>
 
 <script>
-export default {}
+import api from '@/utils/apiClient'
+
+export default {
+  data: () => ({
+    valid: false,
+    name: '',
+    nameRules: [v => !!v || 'E-mail is required'],
+    isLoading: false,
+    lazy: false
+  }),
+  methods: {
+    submit: async function() {
+      if (!this.valid) {
+        return
+      }
+      this.isLoading = true
+      try {
+        const resp = await api.put('/me', {
+          name: this.name
+        })
+        this.$store.dispatch('user/updateUser', resp.data)
+        this.$router.push('/')
+      } catch (e) {
+        console.debug('error')
+      } finally {
+        this.isLoading = false
+      }
+    }
+  }
+}
 </script>
+
+<style scoped>
+.form {
+  width: 300px;
+}
+.submitButton {
+  display: flex;
+  justify-content: center;
+  box-shadow: none;
+}
+.button {
+  width: 100%;
+  box-shadow: none;
+}
+.text {
+  font-size: 16px;
+  margin-bottom: 20px;
+}
+</style>
--- qoLabFrontEnd/store/user.js
+++ qoLabFrontEnd/store/user.js
@@ -2,7 +2,7 @@ export const state = () => ({})
 
 export const mutations = {
   updateUser(state, user) {
-    state = user
+    Object.assign(state, { ...user })
   }
 }
 
